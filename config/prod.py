2ea1befd74e8b04ff50ccd0f71db592c77a927a5
KevinGuzman <guzmankevin420@gmail.com>
2021-12-20 11:12:51 -0600

    Update config

--- config/prod.py
+++ config/prod.py
@@ -1,17 +1,6 @@
 from .default import *
-
-# SECRET_KEY = '823e5dc3ecc3fc5f1ebd185f6717e6a49f8cf179'
+import os
 
 FLASK_ENV = APP_ENV_PRODUCTION
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://u3otevp05vjbg3hn:qkFupmMC3HBFDU941amf@br6ok3dyd1m1fzz5ksdk-mysql.services.clever-cloud.com:3306/br6ok3dyd1m1fzz5ksdk'
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/send_email_test';
-
-print(".")
-print(".")
-print(".")
-print(".")
-print(".")
-print(".")
-print(".")
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
